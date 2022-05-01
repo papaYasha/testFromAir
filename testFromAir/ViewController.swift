eb42a363d58631e0ac119779eacf41d4cfd8145c
MichaelMal1992 <>
2022-05-01 16:44:11 +0300

    init commit

---
+++ testFromAir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testFromAir
+//
+//  Created by Admin on 1.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
